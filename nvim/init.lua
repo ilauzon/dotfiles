814eb798eb0a01662a2e06cd10b48a0dd33de1c0
ilauzon <ilauzonvc@gmail.com>
2026-01-27 16:06:44 -0800

    Add Lua LSP and organize init.lua into functions

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,19 +6,71 @@ require('config.lazy')
 
 -- telescope
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+local function setup_telescope()
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope show keymappings' })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Telescope document symbols' })
+    vim.keymap.set('n', '<leader>fw', builtin.lsp_dynamic_workspace_symbols, { desc = 'Telescope workspace symbols' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
+
+    local telescope = require('telescope')
+    local actions = require('telescope.actions')
+    telescope.setup({
+        defaults = {
+            mappings = {
+                i = {
+                    ['<CR>'] = actions.select_default + actions.center,
+                },
+                n = {
+                    ['<CR>'] = actions.select_default + actions.center,
+                },
+            },
+        },
+        pickers = {
+            find_files = {
+                hidden = true,
+                -- Optional: prevent searching inside the .git directory
+                find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+            },
+        },
+    })
+
+    -- telescope - show projects on startup when called with no args
+
+    local telescope_projects = telescope.extensions.project
+    if vim.fn.argc() == 0 then
+        vim.api.nvim_create_autocmd('VimEnter', {
+            callback = function()
+                telescope_projects.project{ 'full' }
+            end,
+        })
+    end
+
+    vim.keymap.set('n', '<leader>fp', telescope_projects.project, { desc = 'Telescope find projects' })
+end
+
+setup_telescope()
 
 -- colorscheme
 
-vim.cmd.colorscheme "catppuccin-mocha"
+local function setup_colorscheme()
+    vim.cmd.colorscheme "catppuccin-mocha"
+end
+
+setup_colorscheme()
 
 -- lsp
 
-vim.lsp.enable('clangd')
+local function setup_lsps()
+    vim.lsp.enable('clangd')
+    vim.lsp.enable('lua_ls')
+end
+
+setup_lsps()
 
 -- that status bar at the bottom
 
@@ -94,15 +146,18 @@ vim.api.nvim_create_autocmd("VimEnter", {
 
 -- general
 
-vim.o.tabstop = 8
-vim.o.expandtab = true
-vim.o.softtabstop = 0
-vim.o.shiftwidth = 4
-vim.o.smarttab = true
+local function setup_general_settings()
+    vim.o.tabstop = 8
+    vim.o.expandtab = true
+    vim.o.softtabstop = 0
+    vim.o.shiftwidth = 4
+    vim.o.smarttab = true
 
-vim.wo.relativenumber = true
-vim.opt.signcolumn = 'yes'
+    vim.wo.relativenumber = true
+    vim.opt.signcolumn = 'yes'
 
-vim.g.netrw_bufsettings = 'noma nomod nobl ro'
-vim.g.netrw_liststyle = 3 -- expand folders without descending
-vim.g.netrw_banner = 0 -- hide banner
+    vim.g.netrw_bufsettings = 'noma nomod nobl ro'
+    vim.g.netrw_liststyle = 3 -- expand folders without descending
+end
+
+setup_general_settings()
